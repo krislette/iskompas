4b09fd44989d944c96099a02b46740c070c800af
Regina Bonifacio <rejinimikasi06@gmail.com>
2024-11-17 13:29:14 +0800

    Add const constructors to icon widgets

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,30 +12,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(222222),
+      backgroundColor: const Color.fromARGB(0, 16, 17, 16),
       // Adding a curved navigation bar at the bottom of the screen
       bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Color(222222),
-        color: Color.fromARGB(255, 101, 27, 27),
-        animationDuration: Duration(milliseconds: 450),
+        backgroundColor: const Color.fromARGB(0, 16, 17, 16),
+        color: const Color.fromARGB(255, 101, 27, 27),
+        animationDuration: const Duration(milliseconds: 450),
         onTap: (index) {
           print(index);
         },
         items: [
           // Home icon
-          Icon(
+          const Icon(
             Icons.map, 
             size: 30, 
             color: Colors.white
             ), 
           // Saved item icon
-          Icon(
+          const Icon(
             Icons.bookmark, 
             size: 30, 
             color: Colors.white
             ), 
           // Images icon
-          Icon(
+          const Icon(
             Icons.image, 
             size: 30, 
             color: Colors.white
