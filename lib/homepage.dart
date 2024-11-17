23748cec523e4d7fd58ddce06dc4c44f25fb4c8e
Regina Bonifacio <rejinimikasi06@gmail.com>
2024-11-17 12:52:19 +0800

    Add CurvedNavigationBar to HomePage

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      // Adding a curved navigation bar at the bottom of the screen
+      bottomNavigationBar: CurvedNavigationBar(
+        items: [
+          // Navigation bar items will be added here
+        ]),
+    );
   }
 }
