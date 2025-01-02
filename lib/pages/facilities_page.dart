ad36b96e539d014be5eaf992ec37738be9d45647
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-01-02 16:26:17 +0800

    Created a white search bar

--- lib/pages/facilities_page.dart
+++ lib/pages/facilities_page.dart
@@ -6,9 +6,46 @@ class FacilitiesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Facilities Page',
-          style: TextStyle(fontSize: 24, color: Iskolors.colorWhite)),
+    return Scaffold(
+      backgroundColor: Iskolors.colorBlack,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Iskolors.colorBlack, 
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(24),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Search facilities...',
+                  border: InputBorder.none,
+                  icon: Icon(Icons.search, color: Colors.grey),
+                ),
+                onChanged: (value) {
+                  print('Search query: $value');
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
