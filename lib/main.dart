3fa14051617e263fce2d7548842fdb6a75e56807
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-01-13 22:23:22 +0800

    Removed unused import: Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'widgets/navbar.dart';
 import 'pages/splash_screen.dart';
 import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
