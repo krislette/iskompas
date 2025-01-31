4d2a7b818537ff67a4b8a7cb874b858c44779d8f
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-01-31 13:55:32 +0800

    Added onTap handler on search bar

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -6,6 +6,7 @@ class CustomSearchBar extends StatelessWidget {
   final ValueChanged<String>? onChanged;
   final TextEditingController? controller;
   final bool isDarkMode;
+  final VoidCallback? onTap;
 
   const CustomSearchBar({
     super.key,
@@ -13,12 +14,15 @@ class CustomSearchBar extends StatelessWidget {
     this.onChanged,
     this.controller,
     this.isDarkMode = true,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    final textColor = isDarkMode ? Iskolors.colorWhite : Iskolors.colorBlack;
-    final hintColor = isDarkMode ? Iskolors.colorLightGrey : Iskolors.colorGrey;
+    final textColor =
+        isDarkMode ? Iskolors.colorWhite : Iskolors.colorDarkShade;
+    final hintColor =
+        isDarkMode ? Iskolors.colorLightGrey : Iskolors.colorDarkShade;
     final backgroundColor =
         isDarkMode ? Iskolors.colorDarkShade : Iskolors.colorPureWhite;
     final borderColor =
@@ -26,31 +30,38 @@ class CustomSearchBar extends StatelessWidget {
     final shadowColor =
         isDarkMode ? Iskolors.colorTransparent : Iskolors.colorLightShadow;
 
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        border: Border.all(color: borderColor),
-        borderRadius: BorderRadius.circular(24),
-        boxShadow: [
-          BoxShadow(
-            color: shadowColor,
-            spreadRadius: 1,
-            blurRadius: 3,
-          ),
-        ],
-      ),
-      child: TextField(
-        controller: controller,
-        style: TextStyle(color: textColor),
-        cursorColor: textColor,
-        decoration: InputDecoration(
-          hintText: hintText,
-          hintStyle: TextStyle(color: hintColor),
-          border: InputBorder.none,
-          icon: Icon(Icons.search, color: hintColor),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          border: Border.all(color: borderColor),
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor,
+              spreadRadius: 1,
+              blurRadius: 3,
+            ),
+          ],
         ),
-        onChanged: onChanged,
+        child: IgnorePointer(
+          // Ignore pointer events if onTap is provided
+          ignoring: onTap != null,
+          child: TextField(
+            controller: controller,
+            style: TextStyle(color: textColor),
+            cursorColor: textColor,
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: TextStyle(color: hintColor),
+              border: InputBorder.none,
+              icon: Icon(Icons.search, color: hintColor),
+            ),
+            onChanged: onChanged,
+          ),
+        ),
       ),
     );
   }
